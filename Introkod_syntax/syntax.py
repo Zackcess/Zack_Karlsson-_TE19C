b48463d05f1874be4e9972ce3eba754bb0d57129
Zackcess <70264398+Zackcess@users.noreply.github.com>
2020-08-28 09:37:28 +0200

    introduktionskod

---
+++ Introkod_syntax/syntax.py
@@ -0,0 +1,13 @@
+print("Hej Zack")
+
+#skapar en variabel och tilldelar det värdet "hund"
+
+name = "hund"
+
+#print(name)
+print(f"Ditt namn är {name}")
+
+side = float(input("ange kvadratens sida: "))
+are = side**2
+
+print(f:"arean av kvadraten är {area} a.e.")
