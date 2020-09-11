e1227e87cf47204550c6840f9fe471f76f43b30e
Zackcess <70264398+Zackcess@users.noreply.github.com>
2020-09-11 12:33:58 +0200

    Save

---
+++ Räkna_med_python/Räkna me kod.py
@@ -0,0 +1,7 @@
+#Lektion 2 #1
+#a
+Bredd=5
+längd=8
+print("En rektangel har bredden", bredd, "cm och längden", längd, "cm.")
+print("Omkretsen är", 2* (bredd+längd), "centimeter.")
+print("Arean är", längd * bredd, "kvadratcentimeter.")
