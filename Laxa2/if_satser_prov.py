c65df59b463fcbb4b459ef44a9ac67821569e4db
Zackcess <70264398+Zackcess@users.noreply.github.com>
2020-09-18 09:46:45 +0200

    EN uppgift denna lektionen, pga strul med python

---
+++ Laxa2/if_satser_prov.py
@@ -0,0 +1,19 @@
+# import random as rnd
+
+# poang = rnd.randint(0,65)
+
+# klass = ["Kockchun", "Tommy", "Henrik", "Zsofia", "Simon", "Tatiana"]
+
+# if poang > 60:
+#    betyg = "A"
+    
+    
+#print(poang)
+
+
+radie = float(input("Säg en radie på en cirkel: "))
+if radie > 0:
+    area = radie**2 * 3.14
+    omkrets = 2*radie * 3.14
+print("cirkelns area är", area, "cm")
+print("cirkelns omkrets är", omkrets, "cm")
