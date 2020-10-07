4b58352c58e790f0cdb5d463d74bbe85dfd660ba
Zackcess <70264398+Zackcess@users.noreply.github.com>
2020-10-07 14:25:46 +0200

    Läxa forsatser

--- Räkna_med_python/while_satser/While_satser.py
+++ Räkna_med_python/while_satser/While_satser.py
@@ -8,18 +8,30 @@ while s<100:
 print(n)
 
 #2
-print("1+2+3+4+5...+99+100")
-s=0
+s=1
 n=0
-while s<99:
+while s<100:
+    n += s
     s+=2 
-    n += s
-print(n)
+print(f"1+2+5+7..+99+100 = {n}")
 #3
-print("1+1/2+1/4+1/8+1/2**n")
-s=0
-n=0
-while s<1:
-    s+=1/2 
-    n += s
-print(n)
+s=0  #variabl ska funkar som räknarvariabel
+n = 0
+while n<10:
+    s += 2**(-n)
+    n = n+1
+print(f"talet är={s}")
+
+#5
+import random as rnd
+s=rnd.randint (0,100)
+h = 0
+while h != s:
+    h = int(input("Gissa på ett heltal mellan 0-100= "))
+    print("Fel, tyvärr")
+    if h < s:
+        print("För lågt")
+    elif h > s:
+        print("För högt")
+    else:
+        print("Rätt svar")
--- Räkna_med_python/while_satser/while_test.py
+++ Räkna_med_python/while_satser/while_test.py
@@ -1,9 +1,42 @@
-
+'''
 print("1+1/2+1/4+1/8+1/2**n")
-s=1
-n=100
+s=1 #variable ska funkar som räknarvariabel
 i=0
-while s<=1:
-    s/=2**n
-    s = i
-    print(i)
+while s<=2:
+    #n=2**s
+print(n)
+'''
+'''
+n = 0 #Räknarvariabel
+while n < 10:
+    print(n, end=" ")
+    n += 1
+'''
+'''
+summa=0
+n=1
+while n < 10:
+    summa = summa + n
+    n=n+2
+print (f"1+2+3+4+5+6+7+8+9+10 = {summa}")
+'''
+
+
+#How to do random
+'''
+import random as rnd
+
+rnd.radint (1,10)
+'''
+import random as rnd
+s=rnd.randint (0,100)
+h = 0
+while h != s:
+    h = int(input("Gissa på ett heltal mellan 0-100= "))
+    print("Fel, tyvärr")
+    if h < s:
+        print("För lågt")
+    elif h > s:
+        print("För högt")
+    else:
+        print("Rätt svar")
