af97d2945cf4632747220b036549749081ec758d
Zackcess <70264398+Zackcess@users.noreply.github.com>
2021-03-15 13:56:13 +0100

    läxadict

--- merprogrammering/filer/Programmering i verkligheten/läxa.py
+++ merprogrammering/filer/Programmering i verkligheten/läxa.py
@@ -36,17 +36,49 @@ for i in range(10):
     siffor[f"{i}"]=slumptal
 
 print(siffror)
-'''
-#3
+
+#3 
+#Index är både nummer och egenskap, medans pokemon är namnet på pokemonen
 pokemons=[]
 pokedex={}
+index=[]
 with open("pokemonlista.txt", "r") as f1:
     for rad in f1:
         rad=rad.replace("\n", "")
         pokemons.append(rad)
-for i in range (10):
-    index=pokemons[rad]
-print(index)
+#pokemondelen mst använda list comprehension
+new_items = [item for item in pokemons if not item.isdigit()]
 
+print (new_items)
+#index delen
+for n in range(150):
+    for i in pokemons:
+        index.append(pokemons)
+
+
+print (new_items)
 '''
-
+#4
+morsetecken={}
+#öppnar filen morse.txt
+with open("morse.txt", "r") as f1:
+    #går igenom alla rader av filen och delar upp det i key och value. key=bokstav. Value=morsekoden
+    for rad in f1:
+        bokstav=rad[:1]
+        kod=rad[3:]
+        morsetecken[bokstav]=kod
+#funktion som översätter
+def översätt(text):
+    kodad=[]
+    #går igenom varje bokstav i ordet och kollar om det finns i dict
+    for bokstav in user_text:
+        
+        if bokstav in morsetecken:
+            print(bokstav)
+            #hittar value till key och lägger det i lista
+            kodad.append(morsetecken[bokstav])
+            #det är något konstigt så att jag får /n
+            fullt_ord=[bokstav[:-1] for bokstav in kodad]
+    print(fullt_ord)
+user_text= input("Vilket ord vill du får transelterat???").upper() #
+översätt(user_text)
